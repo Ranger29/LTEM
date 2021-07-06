ac773abe1561f48f18ba69ceeff6fc6fc11d4455
ranger iradukunda <you@example.com>
2021-07-06 10:29:55 -0600

    blog up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.title
+    date = models.date
+    summary = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
