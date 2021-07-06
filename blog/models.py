d04d6cb2bc978ca25b11fac55af1cb9943ece83a
ranger iradukunda <you@example.com>
2021-07-06 15:07:08 -0600

    update

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,6 @@ from django.utils import timezone
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField(auto_now_add = True)
+    pub_date = models.DateTimeField()
     summary = models.TextField()
     image = models.ImageField(upload_to='images/')
-
-    def __str__(self):
-        return self.title
